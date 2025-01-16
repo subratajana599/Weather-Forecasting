1b7a53419b9a4c1986d962bc62c81914b69bfd99
subratajana599 <106270697+subratajana599@users.noreply.github.com>
2025-01-16 23:23:00 +0530

    Add files via upload

---
+++ Weather Forecasting/SRClimate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRClimate
+//
+//  Created by https://github.com/guowilling on 16/9/18.
+//  Copyright © 2016年 SR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
